2e56610b601b9a6dd63163bdd6c11ea22477b132
ashuharsh22 <49532763+ashuharsh22@users.noreply.github.com>
2019-11-25 00:36:03 +0530

    Update __init__.py

--- crops/__init__.py
+++ crops/__init__.py
@@ -48,7 +48,7 @@ def getData():
         if (temp_reading>=temp_lower and temp_reading<=temp_upper):
             temp_dev.append(abs(temp_reading-avg_temp))
         else:
-            temp_dev.append(float('inf'))
+            temp_dev.append(float(999))
             
         mois_string=sheet.cell_value(i, 2)
         mois_lower,mois_upper=map(float,mois_string.split('-'))
@@ -56,7 +56,7 @@ def getData():
         if (mois_reading>=mois_lower and mois_reading<=mois_upper):
             mois_dev.append(abs(mois_reading-avg_mois))
         else:
-            mois_dev.append(float('inf'))
+            mois_dev.append(float(999))
             
         hum_string=sheet.cell_value(i, 3)
         hum_lower,hum_upper=map(float,hum_string.split('-'))
@@ -64,13 +64,13 @@ def getData():
         if (hum_reading>=hum_lower and hum_reading<=hum_upper):
             hum_dev.append(abs(hum_reading-avg_hum))
         else:
-            hum_dev.append(float('inf'))
+            hum_dev.append(float(999))
         sum_dev[crop_string]=temp_dev[c]+mois_dev[c]+hum_dev[c]
         c+=1
     print("Suggested Crops: ")
     listofCrops = sorted(sum_dev.items() ,  key=lambda x: x[1])
     for elem in listofCrops :
-        if(elem[1]!=float('inf')):
+        if(elem[1]!=999):
             print(elem[0] , " ::" , elem[1] )
     json_data = {"data": listofCrops}
     return json_data
